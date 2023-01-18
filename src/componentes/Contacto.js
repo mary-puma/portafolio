9cf8fc5f6e583ada88015260c2d2041bc85bf43f
Mary <pumaisabel029@gmail.com>
2023-01-18 16:59:26 -0300

    se agrego sweetalert

--- src/componentes/Contacto.js
+++ src/componentes/Contacto.js
@@ -1,4 +1,5 @@
 import emailjs from '@emailjs/browser'
+import sweetalert2 from 'sweetalert2';
 import '../css/contacto.css'
 
 
@@ -10,6 +11,16 @@ function Contacto() {
             .then(response => console.log(response))
             .catch(error => console.error(error));
 
+
+        sweetalert2.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Enviado',
+            showConfirmButton: false,
+            timer: 2000
+        })
+
+
         e.target.reset();
     }
 
@@ -19,8 +30,8 @@ function Contacto() {
             <div className='div-form' id='contacto' >
                 <h1 className='title-form'>Contacto</h1>
                 <form className='form-email' onSubmit={sendEmail}>
-                    <input type='text' name='user_name' placeholder='Nombre'></input>
-                    <input type='email' name='user_email' placeholder='Email'></input>
+                    <input type='text' name='user_name' required placeholder='Nombre'></input>
+                    <input type='email' name='user_email' required placeholder='Email'></input>
                     <textarea className='box-text' name='user_message' id='' cols='45' rows='8' required placeholder='Mensaje' />
                     <button type='submit'>Enviar</button>
                 </form>
