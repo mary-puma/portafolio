f3b70399674f4c1d04c19602a9ca2ac47db7c918
Mary <pumaisabel029@gmail.com>
2023-01-19 19:55:51 -0300

    responsive mobile

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -4,7 +4,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa'
 function Footer() {
     return (
         <div className="container-footer">
-            <footer className="position-relative start-0 bottom-0 end-0 bg-dark bg-gradient pb-0 ">
+            <footer className="position-relative start-0 bottom-0 end-0 bg-dark pb-0 ">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3 mx-5">
                     <li className="nav-item">
                         <a className="nav-link px-2 text-white" href='https://www.linkedin.com/in/mary-isabel-puma-aa899a20b/' target='_blank' rel="noreferrer noopener">
@@ -15,7 +15,6 @@ function Footer() {
                         <a className="nav-link px-2 text-white" href='https://github.com/mary-puma' target='_blank' rel="noreferrer noopener">
                             <FaGithub className='icon' />
                         </a>
-
                     </li>
 
                 </ul>
